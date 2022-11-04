97bdd4750d28dee5e161c6e78d65c7b16dd45d41
Dhanush9358 <99663577+Dhanush9358@users.noreply.github.com>
2022-11-04 11:26:25 +0530

    Delete ex.js

--- ex.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
- // create express application instance
-var app = express()
-  // express route
-app.get('/', function (req, res) {
-   res.send('This is a Sample Example for Express.js ')
-})
- var server = app.listen(8000) // start server
